4822c703ae22a9a8f7c170109b8a3a7012094fd2
ashish <ashish.koirala004@gmail.com>
2026-07-16 15:04:05 +0545

    commit3

--- src/main/java/io/herald/grp2springweb/Controller/TotalController.java
+++ src/main/java/io/herald/grp2springweb/Controller/TotalController.java
@@ -1,7 +1,10 @@
 package io.herald.grp2springweb.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //Controller-> http request handler like get, post, etc....
 @Controller
@@ -19,6 +22,23 @@ public class TotalController {
         return "signupPage";
     }
 
+    @GetMapping("/login")
+    public String loginGet()
+    {
+        return "loginPage";
+    }
+
+
+    @PostMapping("/loginPost")
+    public String loginPost(HttpServletRequest request)
+    {
+        String username, password;
+        //username=request.getParameter("username");
+username=request.getParameter("username");
+password=request.getParameter("password");
+
+
+    }
 
 
 
